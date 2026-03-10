73073babc2b20e3bc4ceb9fb2dc7ac1a6fe9c2bc
Anuradha <anuanya1234@gmail.com>
2026-03-09 22:00:02 -0400

    added basic ui with study and procastination buttons

--- tracker/src/components/Tracker.jsx
+++ tracker/src/components/Tracker.jsx
@@ -1,7 +1,29 @@
-function Tracker() {
+import React from 'react'
+
+const Tracker = () => {
     return (
-        <div>
-            <h1>Study</h1>
-        </div>
+        <>
+            <div className=' flex justify-center items-center flex-col p-30 mx-6 gap-2  '>
+                <h1 className=' font-stretch-50% font-bold text-black text-3xl  '>Study Vs Distraction Tracker </h1>
+                <div className=' border-[#1f1f1f]  border-[0.5px] w-1/5 my-1'></div>
+                <div className=' my-4 '>
+                    <ul className=' flex flex-col gap-2'>
+
+                        <li className=' font-bold text-xl'> Study time : 0 min </li>
+                        <li className=' font-bold text-red-600 text-xl'>Prcocastination : 0min</li>
+                    </ul>
+                </div>
+                <div className=' flex gap-6'>
+                    <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg cursor-pointer">
+                        Studying
+                    </button>
+                    <button class="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg cursor-pointer">
+                        Procastinating
+                    </button>
+                </div>
+            </div>
+        </>
     )
 }
+
+export default Tracker
