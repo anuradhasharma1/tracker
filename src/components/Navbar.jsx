2318b244a4432156f36c2b872262cbd31ccd183a
Anuradha <anuanya1234@gmail.com>
2026-03-10 17:20:18 -0400

    made navbar responsive to diff screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Navbar = () => {
     return (
         <>
-            <nav className=' bg-[#111] border-b border-[#1f1f1f] h-25 flex items-center justify-center'>
-                <div className='text-white font-stretch-75% font-bold text-4xl mx-6  '>
+            <nav className=' bg-[#111] border-b border-[#1f1f1f] h-16 sm:h-20 md:h-24 flex items-center justify-center px-4'>
+                <div className='text-white font-bold text-2xl sm:text-3xl md:text-4xl mx-4   '>
                     Tracker
                 </div>
-                <img className='invert mt-2  ' src="/tracker.png" alt="" />
+                <img className='invert mt-1 w-7 sm:w-8 md:w-10  ' src="/tracker.png" alt="" />
 
             </nav>
         </>
