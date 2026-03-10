06a3243824ec50cea36107242b32535b5260357c
Anuradha <anuanya1234@gmail.com>
2026-03-10 17:24:58 -0400

    made progress bar responsive

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -7,21 +7,21 @@ const ProgressBar = ({ Study, Lazy }) => {
     const LazyPercent = Math.min((Lazy / MAX) * 100, 100);
 
     return (
-        <div className="w-full max-w-md mt-4 flex flex-col gap-3">
+        <div className="w-full  mt-4 flex flex-col gap-3">
             <div>
-                <div className="w-full bg-gray-200 h-6 rounded-md">
-                    <div className="bg-green-500 h-6 rounded-md transition-all duration-500" style={{ width: `${StudyPercent}%` }}></div>
+                <div className="w-full bg-gray-200 h-4 sm:h-5 md:h-6 rounded-md">
+                    <div className="bg-green-500  h-4 sm:h-5 md:h-6 rounded-md transition-all duration-500" style={{ width: `${StudyPercent}%` }}></div>
                 </div>
-                <p className="text-sm mt-1 text-center font-semibold text-green-600">
+                <p className=" text-xs sm:text-sm mt-1 text-center font-semibold text-green-600">
                     Study: {Math.round(StudyPercent)}% of 30 min goal
                 </p>
             </div>
 
             <div>
-                <div className="w-full bg-gray-200 h-6 rounded-md">
-                    <div className="bg-red-500 h-6 rounded-md transition-all duration-500" style={{ width: `${LazyPercent}%` }}></div>
+                <div className="w-full bg-gray-200 h-4 sm:h-5 md:h-6 rounded-md">
+                    <div className="bg-red-500 h-4 sm:h-5 md:h-6 rounded-md transition-all duration-500" style={{ width: `${LazyPercent}%` }}></div>
                 </div>
-                <p className="text-sm mt-1 text-center font-semibold text-red-600">
+                <p className="text-xs sm:text-sm mt-1 text-center font-semibold text-red-600">
                     Procrastination: {Math.round(LazyPercent)}% 
                 </p>
             </div>
